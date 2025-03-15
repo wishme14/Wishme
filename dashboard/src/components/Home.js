4a31799b30358aee4a50590b438e7bb597ce76d6
Mohd Rihan <wishmeon14@gmail.com>
2025-03-15 17:03:21 +0530

    Pushing initial code of dashboard

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+      <div>
+        <h1>Home App</h1>
+        <p>
+          Dashboard First App.
+        </p>
+      </div>
+    );
+  }
+  
+  export default Home;
